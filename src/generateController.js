c984e8859d7c7fc98ba898badc00dece5517883d
filipestudt <filipestudt@gmail.com>
2020-12-17 13:27:54 -0300

    Make not required pass the models at config

--- src/generateController.js
+++ src/generateController.js
@@ -1,7 +1,13 @@
 module.exports = (route, name, models) => {
-    var modelName = name[0].toUpperCase() + name.slice(1);
-    var content = `const ${modelName} = require('../models/${modelName}');\n\n`;
-    var model = models[modelName];
+    var modelName = '';
+    var model = '';
+    var content = '';
+
+    if (models) {
+        var modelName = name[0].toUpperCase() + name.slice(1);
+        var content = `const ${modelName} = require('../models/${modelName}');\n\n`;
+        var model = models[modelName];
+    }
 
     for (let [key, val] of Object.entries(route)) {
         let action = require('./actions/' + val.action);
