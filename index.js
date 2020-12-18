6fea1ebaeb034a25a6d5a789025e4f3ac6eced99
filipestudt <filipestudt@gmail.com>
2020-12-18 16:13:07 -0300

    Separation to more functions to have more options

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
 const fs = require('fs');
-//const config = require('./config');
+const util = require('util');
 const generateRoute = require('./src/generateRoute');
 const generateController = require('./src/generateController');
+const generateRouteFromModel = require('./src/generateRouteFromModel');
 
-module.exports = (config) => {
-    var routes = fs.readdirSync(config.routesPath);
+exports.generateFromFile = (config) => {
+    /* 
+     * Check for the required parameters 
+     */
+    if (!config.filesPath) {
+        throw 'Missing filesPath argument';
+    }
 
-    for (let route of routes) {
-        let routeObj = require(config.routesPath + '/' + route);
-        let routeStringContent = generateRoute(routeObj, route.replace('.js', ''));
-        let controllerStringContent = generateController(routeObj, route.replace('.js', ''), config.models);
-        fs.writeFileSync(config.routesOutputPath + '/' + route.replace('.js', '-route.js'), routeStringContent);
-        fs.writeFileSync(config.controllersOutputPath + '/' + route.replace('.js', '-controller.js'), controllerStringContent);
+    var files = fs.readdirSync(config.filesPath);
+
+    for (let file of files) {
+        let routeObj = require(config.filesPath + '/' + file);
+
+        if (config.routesOutput) {
+            let routeStringContent = generateRoute(routeObj, file.replace('.js', ''), config.consign);
+            fs.mkdirSync(config.routesOutput, { recursive: true });
+            fs.writeFileSync(config.routesOutput + '/' + file.replace('.js', '-route.js'), routeStringContent);
+        }
+
+        if (config.controllersOutput) {
+            let controllerStringContent = generateController(routeObj, file.replace('.js', ''), config.models);
+            fs.mkdirSync(config.controllersOutput, { recursive: true });
+            fs.writeFileSync(config.controllersOutput + '/' + file.replace('.js', '-controller.js'), controllerStringContent);
+        }
+    }
+}
+
+/**
+ * @description
+ * Generate default routes from the model
+ * If the model have primary key, will generate a getById method
+ * If the model have foreign key, will generate a method for each foreign key
+ * 
+ * Expected params:
+ * {
+ *  models: db.models,
+ *  routesOutput: '',
+ *  controllersOutput: ''
+ * }
+ * @param {Object} config 
+ */
+exports.generateFromModel = (config) => {
+    if (!config.models) {
+        throw 'Missing models argument';
+    }
+
+    let obj = generateRouteFromModel(config.models);
+
+    for (let [key, val] of Object.entries(obj)) {
+        if (config.routesOutput) {
+            let routeStringContent = generateRoute(val, key[0].toLocaleLowerCase() + key.slice(1), config.consign);
+            fs.mkdirSync(config.routesOutput, { recursive: true });
+            fs.writeFileSync(config.routesOutput + '/' + key[0].toLocaleLowerCase() + key.slice(1) + '-route.js', routeStringContent);
+        }
+
+        if (config.controllersOutput) {
+            let controllerStringContent = generateController(val, key[0].toLocaleLowerCase() + key.slice(1), config.models);
+            fs.mkdirSync(config.controllersOutput, { recursive: true });
+            fs.writeFileSync(config.controllersOutput + '/' + key[0].toLocaleLowerCase() + key.slice(1) + '-controller.js', controllerStringContent);
+        }
+    }
+}
+
+
+exports.generateFileFromModel = (config) => {
+    if (config.defaultRoutes) {
+        let obj = generateRouteFromModel(config.models);
+        for (let [key, val] of Object.entries(obj)) {
+            fs.writeFileSync(config.configFileOutputPath + '/' + key[0].toLocaleLowerCase() + key.slice(1) + '.js', 'module.exports = ' + util.inspect(val), 'utf-8');
+        }
     }
 }
