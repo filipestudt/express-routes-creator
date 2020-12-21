eea1db7e5bddc58e0935ae540798a19ce3a4e48a
filipestudt <filipestudt@gmail.com>
2020-12-21 14:37:47 -0300

    Create post action

---
+++ src/actions/post.js
@@ -0,0 +1,31 @@
+module.exports = (modelName, model) => {
+    var ownerField = '';
+    var str = '\n\ttry {';
+
+    if (model) {
+        for (let [key, value] of Object.entries(model.tableAttributes)) {
+            if (value.owner == true) {
+                ownerField = key;
+                break;
+            }
+        }
+    }
+
+    if (ownerField) {
+        str += `\n\t\treq.body[${ownerField}] = await getUserIdByToken(req, res);`;
+    }
+
+    str += `
+        const response = await ${modelName}.create(req.body);
+        res.status(201).send({
+            message: 'Operação efetuado com sucesso'
+        });
+    }
+    catch (e) {
+        res.status(500).send({
+            message: 'Unexpected error'
+        })
+    }`
+
+    return str;
+}
